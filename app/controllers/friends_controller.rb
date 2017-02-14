cac851914595a1f12c82c843147511176085c3d4
Wen Zhang <iwenyou@gmail.com>
2017-02-13 21:49:29 -0800

    create added

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -15,13 +15,24 @@ class FriendsController < ApplicationController
 
   def new
 
-    
-
   end
 
   def create
 
+    @friend = Unirest.post("http://localhost:3000/api/v1/friends", :parameters => {first_name: params[:first_name], last_name: params[:last_name], cat_name: params[:cat_name]}).body
+
+    redirect_to "/friends/#{@friend['id']}"
+
+  end
+
+  def update
 
 
+  end
+
+  def destroy
+    @friend = Unirest.delete("http://localhost:3000/api/v1/friends/#{id}").body
+
+    render '/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/friends/:id' => 'friends#show'
-  # get '/friends' => 'friends#index'
+  get '/friends' => 'friends#index'
+  get '/friends/new' => 'friends#new'
+  post '/friends' => 'friends#create'
+  get '/friends/:id/edit' => 'friend#edit'
+  patch '/friends/:id' => 'friends#update'
+  delete '/friend/:id' => 'friends#destroy'
+  get '/friends/:id' => 'friends#show'
 
-  resource :friends
+
 end
